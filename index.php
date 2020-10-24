f5bf86cd87e272bc26afb4b3b5b6755b27cf975b
michaellalastima <60122577+michaellalastima@users.noreply.github.com>
2020-10-24 22:15:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,134 @@
-<!DOCTYPE html>
-<html>
-<body onload="loadXMLDoc()">
-
-<h2>Using the XMLHttpRequest Object</h2>
-
-<div id="demo">
-</div>
-
-<script>
-function loadXMLDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "emails.xml", true);
-  xhttp.send();
-}
-</script>
-</body>
-</html>
+<g>
+<svg viewBox="0 0 900 780" >
+  <circle cx="150" cy="150" r="150"  fill="#B9B9B1"/>
+   <circle cx="240" cy="148" r="110"  fill="white"/>
+   <text x="110" y="10" style="fill:#FAF785" font-size="10px"> 
+      since when the moon
+      </text>
+  <text x="90" y="20" style="fill:#FAF785" font-size="9px"> 
+     and Earth this close to each other?
+      </text>
+   <text x="68" y="28" style="fill:#FAF785" font-size="8px"> 
+      you shine just by existing
+Can I stay by your side?
+      </text>
+  <text x="62" y="38" style="fill:#FAF785" font-size="10px"> 
+   You are my Earth, To you I am just a moon
+      </text>
+  <text x="47" y="48" style="fill:#FEDF06" font-size="9px"> 
+   Your little star that brightens your heart
+      </text>
+  <text x="39" y="58" style="fill:#FEDF06" font-size="8.7px"> 
+   You are my Earth, And all I see is you
+      </text>
+  <text x="32" y="68" style="fill:#FEDF06" font-size="10px"> 
+   I am just gazing at you like this
+      </text>
+  <text x="26" y="78" style="fill:#F9E349" font-size="9px"> 
+ Everyone says I am beautiful but
+      </text>
+  <text x="18" y="88" style="fill:#F2E060" font-size="11px"> 
+  My oceans are all pitch-black
+      </text>
+  <text x="15" y="95" style="fill:#F2E060" font-size="6px"> 
+ A star where flowers bloom and the sky is deep-blue
+      </text>
+  <text x="10" y="103" style="fill:#F2E060" font-size="9px"> 
+  The one that's truly beautiful is you
+      </text>
+   <text x="9" y="110" style="fill:#F2E060" font-size="5px"> 
+  I wonder all of a sudden, are you also looking at me right now?
+      </text>
+   <text x="5" y="114" style="fill:#F2E060" font-size="5px"> 
+  Even my painful scars, wouldn't everything be found out by you?
+      </text>
+   <text x="4" y="122" style="fill:#E4DF35" font-size="8px"> 
+  I will orbit you I will stay by your side
+      </text>
+  <text x="4" y="130" style="fill:#E4DF35" font-size="8.5px"> 
+  I will become your light, All for you
+      </text>
+  <text x="1" y="138" style="fill:#E4DF35" font-size="6px"> 
+  I didn't even have a name
+Until the moment I met you
+      </text>
+  <text x="3" y="145" style="fill:#E4DF35" font-size="6.5px"> 
+ You gave me love and,
+Now became my reason
+      </text>
+   <text x="0.1" y="153" style="fill:#E4DF35" font-size="7.5px"> 
+ You are my Earth,
+To you I am just a moon
+      </text>
+   <text x="2" y="160" style="fill:#F9E349" font-size="8px"> 
+ Your little star that brightens your heart
+      </text>
+   <text x="2" y="168" style="fill:#F9E349" font-size="8.5px"> 
+ You are my Earth, And all I see is you
+      </text>
+  <text x="5" y="178" style="fill:#F2E060" font-size="10px"> 
+ I am just gazing at you like this
+      </text>
+  <text x="6" y="186" style="fill:#F2E060" font-size="12px"> 
+ In the crescent moon night
+      </text>
+  <text x="8" y="193" style="fill:#F2E060" font-size="6px"> 
+ Even after closing my eyes you rush in with your blue
+      </text>
+  <text x="18" y="203" style="fill:#FEF839" font-size="12px"> 
+ In the full moon night
+      </text>
+  <text x="13" y="210" style="fill:#FEF839" font-size="5px"> 
+ Would it be okay to hold you inside my eyes after opening them?
+      </text>
+  <text x="15" y="216" style="fill:#FEF839" font-size="5.4px"> 
+ I wonder all of a sudden, are you also looking at me right now?
+      </text>
+  <text x="17" y="220" style="fill:#FEF839" font-size="5.3px"> 
+ Even my painful scars, wouldn't everything be found out by you?
+      </text>
+  <text x="20" y="225" style="fill:#FEF839" font-size="4.4px"> 
+ I will orbit you,
+I will stay by your side,
+I will become your light,
+All for you.
+      </text>
+  <text x="23" y="230" style="fill:#FEF839" font-size="5px"> 
+ Even in the light of day,
+Even in the dark of night,
+    You stay by my side
+      </text>
+  <text x="28" y="235" style="fill:#FEF839" font-size="5px"> 
+ Even when I am sad,
+Even when I am hurt,
+You simply shine on me.
+      </text>
+  <text x="33" y="241" style="fill:#F2E060" font-size="6.5px"> 
+ Rather than saying anything,
+Rather than thanking you
+      </text>
+  <text x="38" y="248" style="fill:#F2E060" font-size="8px"> 
+ I will stay by your side
+In the pitch-dark night
+      </text>
+  <text x="43" y="255" style="fill:#F2E060" font-size="7.5px"> 
+ I will protect that place next to you
+More brightly
+      </text>
+  <text x="53" y="265" style="fill:#F2E060" font-size="8.5px"> 
+ I wonder all of a sudden, do you really know yourself?
+      </text>
+  <text x="64" y="273" style="fill:#ECEC1F" font-size="8.5px"> 
+ Do you know how pretty you are?
+I will orbit you
+      </text>
+  <text x="93" y="281" style="fill:#ECEC1F" font-size="9.5px"> 
+ I will stay by your side
+      </text>
+  <text x="101" y="288" style="fill:#ECEC1F" font-size="8.5px"> 
+  I will become your light
+      </text>
+  <text x="119" y="296" style="fill:#ECEC1F" font-size="10px"> 
+All for you
+      </text>
+  </svg>
